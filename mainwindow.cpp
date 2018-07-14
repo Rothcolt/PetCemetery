5ab58613606b77af085df594ee7c446f8dbd1c93
tokiiiii <40616791+tokiiiii@users.noreply.github.com>
2018-07-14 04:06:46 -0700

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_login_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_2_password->text();
+
+    if(username == "poop" && password == "12345")
+    {
+        QMessageBox::information(this, "Login", "Username and password is correct");
+        hide(); // This will hide the login window after entering in correct login
+        secDialog = new SecDialog(this);
+        secDialog->show();
+    }
+    else
+    {
+        QMessageBox::information(this, "Login","Username and password is not correct");
+    }
+
+}
