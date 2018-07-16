c872ab5018b1b9b054e64b92d8bc81d829f891ee
Rothcolt <mw.rothcolt@gmail.com>
2018-07-15 22:54:07 -0700

    Added Doxygen comments to the polygon class. Cleaned up the code a little bit.

--- polygon.cpp
+++ polygon.cpp
@@ -1,7 +1,7 @@
 #include "polygon.h"
 
-Polygon::Polygon()
-       :Shape()
+//! Default constructor.
+Polygon::Polygon() : Shape()
 {
     x1 = 0;
     x2 = 0;
@@ -11,6 +11,7 @@ Polygon::Polygon()
     y3 = 0;
 }
 
+//! Paramterized constructor with all variables.
 Polygon::Polygon(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2, int x3, int y3)
 {
     this->x1 = x1;
@@ -24,36 +25,47 @@ Polygon::Polygon(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2,
     this->id = id;
 }
 
-Polygon::~Polygon()
-{
+//! Default destructor.
+Polygon::~Polygon() {
 
+    return;
 }
 
-void Polygon::drawShape(QPainter* painter)
-{
-    // Uses pointer to access and invoke QPainter object drawPolygone function
-//    painter->drawPolygon(this, Qt::OddEvenFill);
+//! Draw the shape in the paint module.
+void Polygon::drawShape(QPainter* painter) {
+
+    // Uses pointer to access and invoke QPainter object drawPolygone function.
+    // painter->drawPolygon(this, Qt::OddEvenFill);
+
+    return;
 }
 
-void Polygon::moveShape()
-{
-    // Resets polygon coordinates
-//    setPoints(3, x1, y1, x2, y2, x3, y3);
+//! Change coordinates and move shape.
+void Polygon::moveShape() {
+
+    // Resets polygon coordinates.
+    // setPoints(3, x1, y1, x2, y2, x3, y3);
+
+    return;
 }
 
-//float Polygon::calcPerimShape()
-//{
-//    // Returns the result of the 3 distances between the 3 points
-//    return
-//    sqrt(pow((static_cast<float>(x1) + static_cast<float>(x2)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y2)), (2.0))) +
-//    sqrt(pow((static_cast<float>(x1) + static_cast<float>(x3)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y3)), (2.0))) +
-//    sqrt(pow((static_cast<float>(x2) + static_cast<float>(x3)), (2.0)) - pow((static_cast<float>(y2) + static_cast<float>(y3)), (2.0)));
+//! Return perimeter (not required).
+/* float Polygon::calcPerimShape() {
 
-//}
+    // Returns the result of the 3 distances between the 3 points.
 
-//float Polygon::calcAreaShape()
-//{
-//    // Area = (perimeter * apothem) / 2
-//    // Apothem = length from midpoint to center of the polygon
-//    return 0;
-//}
+    return
+    sqrt(pow((static_cast<float>(x1) + static_cast<float>(x2)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y2)), (2.0))) +
+    sqrt(pow((static_cast<float>(x1) + static_cast<float>(x3)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y3)), (2.0))) +
+    sqrt(pow((static_cast<float>(x2) + static_cast<float>(x3)), (2.0)) - pow((static_cast<float>(y2) + static_cast<float>(y3)), (2.0)));
+
+} */
+
+//! Return area (not required).
+/* float Polygon::calcAreaShape() {
+
+    // Area = (perimeter * apothem) / 2
+    // Apothem = length from midpoint to center of the polygon
+
+    return 0;
+}
--- polygon.h
+++ polygon.h
@@ -3,28 +3,32 @@
 
 #include "shape.h"
 
-class Polygon: public Shape
-{
+/*! Derived from abstract base class Shape. */
+class Polygon: public Shape {
 public:
-    // **** CONSTRUCTORS ***************** //
-    Polygon();
-    Polygon(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2, int x3, int y3);
-    // **** DESTRUCTOR ***************** //
-    virtual ~Polygon();
-    // **** VIRTUAL ***************** //
-    virtual void drawShape(QPainter* painter);
-    virtual void moveShape();
-//    virtual float calcPerimShape();
-//    virtual float calcAreaShape();
-    // **** ACCESSOR ***************** //
-    // **** MUTATOR ***************** //
+
+    /*! Polygon object, with corner variables.
+     * Able to be drawn and moved. */
+
+    Polygon();                                          /*! Default constructor.      */
+    Polygon(QPen pen, QBrush brush, int id, int x1,     /*! Paramterized constructor  */
+            int y1, int x2, int y2, int x3, int y3);    /*! with all variables.       */
+    virtual ~Polygon();                                 /*! Default destructor.       */
+
+    virtual void drawShape(QPainter* painter);          /*! Draw the shape in the paint module. */
+    virtual void moveShape();                           /*! Change coordinates and move shape.  */
+/*  virtual float calcPerimShape(); */                  /*! Return perimeter (not required).    */
+/*  virtual float calcAreaShape();  */                  /*! Return area (not required).         */
+
 private:
-    int x1;
-    int x2;
-    int x3;
-    int y1;
-    int y2;
-    int y3;
+
+    int x1;     /*! Position Variable : Holds first x-coordinate for polygon corner.  */
+    int x2;     /*! Position Variable : Holds first y-coordinate for polygon corner.  */
+    int x3;     /*! Position Variable : Holds second x-coordinate for polygon corner. */
+    int y1;     /*! Position Variable : Holds second y-coordinate for polygon corner. */
+    int y2;     /*! Position Variable : Holds third x-coordinate for polygon corner.  */
+    int y3;     /*! Position Variable : Holds third y-coordinate for polygon corner.  */
+
 };
 
 #endif // POLYGON_Ha
