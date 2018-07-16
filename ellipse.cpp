c329574e66d9896825f7873f21d6d8e700999cd9
Rothcolt <mw.rothcolt@gmail.com>
2018-07-15 21:19:37 -0700

    Doxygen comments added to ellipse files. Code cleaned up a bit.

--- ellipse.cpp
+++ ellipse.cpp
@@ -1,16 +1,19 @@
 #include "ellipse.h"
 
-Ellipse::Ellipse()
-    :Shape()
-{
+//! Default constructor.
+Ellipse::Ellipse() :Shape() {
+
     x1 = 0;
     y1 = 0;
     a = 0;
     b = 0;
+
+    return;
 }
 
-Ellipse::Ellipse(QPen pen, QBrush brush, int id, int x1, int y1, int a, int b)
-{
+//! Paramterized constructor with all variables.
+Ellipse::Ellipse(QPen pen, QBrush brush, int id, int x1, int y1, int a, int b) {
+
     this->x1 = x1;
     this->y1 = y1;
     this->a = a;
@@ -18,39 +21,53 @@ Ellipse::Ellipse(QPen pen, QBrush brush, int id, int x1, int y1, int a, int b)
     this->pen = QPen(pen);
     this->brush = QBrush(brush);
     this->id = id;
+
+    return;
 }
 
-Ellipse::~Ellipse()
-{
+//! Default destructor.
+Ellipse::~Ellipse() {
 
+    return;
 }
 
-void Ellipse::drawShape(QPainter* painter)
-{
+//! Draw the shape in the paint module.
+void Ellipse::drawShape(QPainter* painter) {
+
     painter->drawEllipse(x1, y1, a, b);
+
+    return;
 }
 
-void Ellipse::moveShape()
-{
+//! Change coordinates and move shape.
+void Ellipse::moveShape() {
+
     // Resets rectangle coordinates
-//    rectangle.moveTo(x1, y1);
+    // rectangle.moveTo(x1, y1);
+
+    return;
 }
 
-//float Ellipse::calcPerimShape()
-//{
-////    float a = (static_cast<float>(ptr->width) / 2);
-////    float b = (static_cast<float>(ptr->height) / 2);
-//    float pi = 3.14159265;
+//! Return perimeter (not required).
+/* float Ellipse::calcPerimShape() {
 
-//    // Returns the result of the ellipse perimeter approximation by the Indian Mathematician Ramanujan
-//    return pi * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
-//}
+    float a = (static_cast<float>(ptr->width) / 2);
+    float b = (static_cast<float>(ptr->height) / 2);
+    float pi = 3.14159265;
 
-//float Ellipse::calcAreaShape()
-//{
-////    float a = (static_cast<float>(ptr->width) / 2);
-////    float b = (static_cast<float>(ptr->height) / 2);
-//    float pi = 3.14159265;
-//    // Returns the result of the ellipse area formula
-//    return a * b * pi;
-//}
+    // Returns the result of the ellipse perimeter approximation by the Indian Mathematician Ramanujan
+    return pi * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
+
+} */
+
+//! Return area (not required).
+/* float Ellipse::calcAreaShape() {
+
+    float a = (static_cast<float>(ptr->width) / 2);
+    float b = (static_cast<float>(ptr->height) / 2);
+    float pi = 3.14159265;
+
+    // Returns the result of the ellipse area formula
+    return a * b * pi;
+
+} */
