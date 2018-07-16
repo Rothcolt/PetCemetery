9013c48883896701d141ce8ac0b7409571cc7f1b
Rothcolt <mw.rothcolt@gmail.com>
2018-07-15 21:04:02 -0700

    Added Doxygen comments to ellipse.h.

--- ellipse.h
+++ ellipse.h
@@ -3,24 +3,28 @@
 
 #include "shape.h"
 
-class Ellipse: public Shape
-{
+/*! Derived from abstract base class Shape. */
+class Ellipse: public Shape {
 public:
-    // **** CONSTRUCTORS ***************** //
-    Ellipse();
-    Ellipse(QPen pen, QBrush brush, int id, int x1, int y1, int a, int b);
-    // **** DESTRUCTOR ***************** //
-    virtual ~Ellipse();
-    // **** VIRTUAL ***************** //
-    virtual void drawShape(QPainter* painter);
-    virtual void moveShape();
-//    virtual float calcPerimShape();
-//    virtual float calcAreaShape();
+
+    /*! Ellipse object, with position and axis variables.
+     * Able to be drawn and moved. */
+
+    Ellipse();                                  /*! Default constructor.      */
+    Ellipse(QPen pen, QBrush brush, int id,     /*! Paramterized constructor  */
+            int x1, int y1, int a, int b);      /*! with all variables.       */
+    virtual ~Ellipse();                         /*! Default destructor.       */
+
+    virtual void drawShape(QPainter* painter);  /*! Draw the shape in the paint module. */
+    virtual void moveShape();                   /*! Change coordinates and move shape.  */
+/*  virtual float calcPerimShape(); */          /*! Return perimeter (not required).    */
+/*  virtual float calcAreaShape();  */          /*! Return area (not required).         */
+
 private:
-    int x1; // Position Variable : Holds x-coordinate for text box
-    int y1; // Position Variable : Holds y-coordinate for text box
-    int a;  // Semi major axis
-    int b;  // Semi minor axis
+    int x1;     /*! Position Variable : Holds x-coordinate for text box. */
+    int y1;     /*! Position Variable : Holds y-coordinate for text box. */
+    int a;      /*! Semi major axis - variable exclusive to ellipses.    */
+    int b;      /*! Semi minor axis - variable exclusive to ellipses.    */
 };
 
 #endif // ELLIPSE_H
