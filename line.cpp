07f6faec38b47d6de90d5de12ef599fc31a7e331
Rothcolt <mw.rothcolt@gmail.com>
2018-07-15 22:09:50 -0700

    Doxygen comments added to line class. Modified some placeholder code.

--- line.cpp
+++ line.cpp
@@ -1,17 +1,20 @@
 #include "line.h"
 
-Line::Line()
-    :Shape()
-{
+//! Derived from abstract base class Shape.
+Line::Line() : Shape() {
+
     this->x1 = 0;
     this->y1 = 0;
     this->x2 = 0;
     this->y2 = 0;
     this->id = 0;
+
+    return;
 }
 
-Line::Line(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2)
-{
+//! Paramterized constructor with all variables.
+Line::Line(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2) {
+
     this->x1 = x1;
     this->y1 = y1;
     this->x2 = x2;
@@ -19,31 +22,43 @@ Line::Line(QPen pen, QBrush brush, int id, int x1, int y1, int x2, int y2)
     this->id = id;
     this->pen = QPen(pen);
     this->brush = QBrush(brush);
+
+    return;
 }
 
-Line::~Line()
-{
+//! Default destructor.
+Line::~Line() {
 
+    return;
 }
 
-void Line::drawShape(QPainter* painter)
-{
+//! Draw the shape in the paint module.
+void Line::drawShape(QPainter* painter) {
+
     painter->drawLine(x1, y1, x2, y2);
+
+    return;
 }
 
-void Line::moveShape()
-{
+//! Change coordinates and move shape.
+void Line::moveShape() {
+
     // Uses line method to reset position
-//    line.setLine(xI, yI, xII, yII);
+    // line.setLine(xI, yI, xII, yII);
+
+    return;
 }
 
-//float Line::calcPerimShape()
-//{
-//    // Returns the result of the distance formula
-//    return sqrt(pow((static_cast<float>(x1) + static_cast<float>(x2)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y2)), (2.0)));
-//}
+//! Return perimeter (not required).
+/* float Line::calcPerimShape() {
 
-//float Line::calcAreaShape()
-//{
-//    return 0.0;
-//}
+    // Returns the result of the distance formula
+    return sqrt(pow((static_cast<float>(x1) + static_cast<float>(x2)), (2.0)) - pow((static_cast<float>(y1) + static_cast<float>(y2)), (2.0)));
+
+} */
+
+//! Return area (not required).
+/* float Line::calcAreaShape() {
+
+    return 0.00;
+} */
